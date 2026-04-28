c68f18f879d8f9159047ba8abb59255f9323ff2c
Zeynep Karaca <zeynneppkaraca@gmail.com>
2026-04-28 15:51:15 +0300

    Add basic chatbot + docker + ollama setup

--- webapp/chat/views.py
+++ webapp/chat/views.py
@@ -1,8 +1,40 @@
-from django.http import HttpResponse, JsonResponse
-from .models import PageContent
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.db.models import Q
+from .models import PageContent, ChatMessage
+
+
+def find_relevant_content(question):
+    words = question.lower().split()
+
+    query = Q()
+
+    for word in words:
+        query |= Q(content__icontains=word)
+        query |= Q(title__icontains=word)
+
+    return PageContent.objects.filter(query)
+
 
 def home(request):
-    return HttpResponse("ACU AI Chatbot is running.")
+    question = request.GET.get("q", "")
+    answer = ""
+
+    if question:
+        results = find_relevant_content(question)
+
+        if results.exists():
+            answer = results.first().content
+        else:
+            answer = "No relevant information found."
+
+        ChatMessage.objects.create(question=question, answer=answer)
+
+    return render(request, "chat/home.html", {
+        "question": question,
+        "answer": answer,
+    })
+
 
 def chat(request):
     question = request.GET.get("q", "")
@@ -12,12 +44,14 @@ def chat(request):
             "error": "Please provide a question using ?q=..."
         })
 
-    results = PageContent.objects.filter(content__icontains=question)
+    results = find_relevant_content(question)
 
     if results.exists():
         answer = results.first().content
     else:
         answer = "No relevant information found."
+
+    ChatMessage.objects.create(question=question, answer=answer)
 
     return JsonResponse({
         "question": question,
