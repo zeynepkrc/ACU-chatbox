b88717d2b50c9ce9e0d2b96601e1489c606d80c7
Zeynep Karaca <zeynneppkaraca@gmail.com>
2026-05-04 22:55:10 +0300

    refactor: restructure backend architecture

--- webapp/chat/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import PageContent, ChatMessage
-
-admin.site.register(PageContent)
-admin.site.register(ChatMessage)
--- webapp/chat/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import chat, home
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('chat/', chat, name='chat'),
-]
