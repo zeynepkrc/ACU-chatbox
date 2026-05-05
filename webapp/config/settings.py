7d0043ef14befb37e384e8b0b1e736f8868e4a1b
Zeynep Karaca <zeynneppkaraca@gmail.com>
2026-05-05 05:44:34 +0300

    Fix Django admin static files

--- webapp/config/settings.py
+++ webapp/config/settings.py
@@ -79,7 +79,7 @@ TIME_ZONE = "Europe/Istanbul"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
